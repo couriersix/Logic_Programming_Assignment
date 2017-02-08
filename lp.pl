4cbc490fb03a2572d5bdabe1369a4915e191d4ac
couriersix <johnathan.curry@bison.howard.edu>
2017-02-08 15:54:41 -0500

    1.5 with comments

--- lp.pl
+++ lp.pl
@@ -1,16 +1,17 @@
 /*1*/
-sum-up-numbers-simple([], 0). 
-sum-up-numbers-simple([Head|Tail], X):-
-	number(Head), 
-	sum-up-numbers-simple(Tail, Sums), 
-	X is Head + Sums.
+sum-up-numbers-simple([], 0). /*declaring the function*/
+sum-up-numbers-simple([Head|Tail], X):- /*define the function*/
+	number(Head), /*check if head is a number*/
+	sum-up-numbers-simple(Tail, Sums), /*Recursing the function, iterating through the rest of the list*/
+	X is Head + Sums. /*Adds value to the total sum*/
 
 sum-up-numbers-simple([Head|Tail], X):- 
-	\+number(Head;Tail), 
-	sum-up-numbers-simple(Tail, X). 
+	\+number(Head;Tail), /*if the value is not a number...*/
+	sum-up-numbers-simple(Tail, X). /*the list iterates to the next value*/
 
 /*2*/
 sum-up-numbers-general([],0).
+sum-up-numbers-general([[]],0).
 sum-up-numbers-general([Head|Tail, X]):-
 	number(Head),
 	sum-up-numbers-general(Tail, Sums),
@@ -20,4 +21,5 @@ sum-up-numbers-general([Head|Tail], X):-
 	\+number(Head;Tail),
 	sum-up-numbers-general(Tail, X).
 
-	
+sum-up-numbers-general
+
