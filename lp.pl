4f24431a660db38a333a763121b5ce546ec96807
couriersix <johnathan.curry@bison.howard.edu>
2017-02-08 17:14:28 -0500

    4.3

--- lp.pl
+++ lp.pl
@@ -32,7 +32,10 @@ sum-up-numbers-general([Head|Tail], X):- /*same as 1*/
 common-unique-elements([L1], [L2], N).
 common-unique-elements([L1], [L2], N):-
 	member(L2, X),!, common-unique-elements([L1], [L2], [L3]).
+
 common-unique-elements([L1], [L2], [L3]):-
 	common-unique-elements([L1], [L2], [L3]).
 
-	
+	member([Head|Tail], X).
+	member([Head|Tail, X]):-
+		member(Tail, X).
