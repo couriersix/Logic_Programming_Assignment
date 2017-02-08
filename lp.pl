d9e279630d73f83cafb7feb8502a3bdfc9a7fe78
couriersix <johnathan.curry@bison.howard.edu>
2017-02-08 15:13:53 -0500

    2.1

--- lp.pl
+++ lp.pl
@@ -1,12 +1,23 @@
 /*1*/
-sum-up-numbers-simple([], 0).
+sum-up-numbers-simple([], 0). 
 sum-up-numbers-simple([Head|Tail], X):-
 	number(Head), 
 	sum-up-numbers-simple(Tail, Sums), 
 	X is Head + Sums.
 
-sum-up-numbers-simple([Head|Tail], X):-
+sum-up-numbers-simple([Head|Tail], X):- 
 	\+number(Head;Tail), 
-	sum-up-numbers-simple(Tail, X).
+	sum-up-numbers-simple(Tail, X). 
 
+/*2*/
+sum-up-numbers-general([],0).
+sum-up-numbers-general([Head|Tail, X]):-
+	number(Head),
+	sum-up-numbers-general(Tail, Sums),
+	X is Head + Sums.
 
+sum-up-numbers-general([Head|Tail], X):-
+	\+number(Head;Tail),
+	sum-up-numbers-general(Tail, X).
+
+	
